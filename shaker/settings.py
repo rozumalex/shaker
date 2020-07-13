eb1ecf731360a8186745bd48e3132839b98a3c51
Alex Rozum <rozumalex@gmail.com>
2020-07-13 19:54:05 +0300

    Edit radio

--- shaker/settings.py
+++ shaker/settings.py
@@ -128,9 +128,9 @@ public_root = root.path('public/')
 MEDIA_ROOT = public_root('media/')
 MEDIA_URL = env.str('MEDIA_URL', default='/media/')
 
+
 STATIC_ROOT = public_root('static/')
 STATIC_URL = env.str('STATIC_URL', default='/static/')
-
 
 LOGIN_REDIRECT_URL = 'radio:index'
 LOGOUT_REDIRECT_URL = 'radio:index'
