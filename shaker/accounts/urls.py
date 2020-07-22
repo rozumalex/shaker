c85d87d5a6adcc9fc70dd060ec5d70c6089dcb74
Alex Rozum <rozumalex@gmail.com>
2020-07-22 12:51:22 +0300

    Add profile page

--- shaker/accounts/urls.py
+++ shaker/accounts/urls.py
@@ -7,5 +7,5 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
-    # path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
--- shaker/accounts/views.py
+++ shaker/accounts/views.py
@@ -2,9 +2,27 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.shortcuts import reverse
+from django.contrib.auth.models import User
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class ProfileView(generic.ListView):
+    model = User
+    template_name = 'accounts/profile.html'
+    context_object_name = 'tracks_list'
+    paginate_by = 15
+    ordering = ['-artist', '-title']
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.user_uploaded.all()
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        kwargs['count_tracks'] = user.user_uploaded.count()
+        return super(ProfileView, self).get_context_data(**kwargs)
